462969d4cd2bfa33e7aea265320c9e0e312e02bf
Юра Гуцалюк <hutsaluck@gmail.com>
2025-02-06 23:05:18 +0200

    Removed the .idea folder

---
+++ src/app/users/layout.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Metadata} from "next";
+
+export const metadata: Metadata = {
+    title: 'metadata'
+}
+
+type Props = { children: React.ReactNode }
+const  = ({children}: Props) => {
+    return (
+        <>
+            
+            </>
+    )
+}
